2c2fd4f1464e3f0dfd0215376b106f872df7a780
Lukas Holota <l.holota@outlook.com>
2020-06-30 19:48:13 +0200

    fix

--- fs/etc/cont-init.d/50-leases.sh
+++ fs/etc/cont-init.d/50-leases.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/with-contenv ash
 
-adduser nonroot dhcp
-
 if [ ! -f "/leases/dhcp.leases" ]; then
   touch "/leases/dhcp.leases"
 fi
 
-# TODO: Try to add $PUID to dhcp group - does it grant any permissions?
-
-#chown -R "root:root" "/leases/dhcp.leases"
 chown -R "$PUID:$PGID" "/leases"
 chown -R "$PUID:$PGID" "/var/lib/dhcp"
